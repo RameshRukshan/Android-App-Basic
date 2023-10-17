f7d2d67a3bf14c96de408362e8d0aa78b5dedf08
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-10-17 20:31:50 +0530

    added back end

--- app/src/main/java/com/example/androidappdemol1/MainActivity.kt
+++ app/src/main/java/com/example/androidappdemol1/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.androidappdemol1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var username = findViewById<TextView>(R.id.txt_username)
+        var password = findViewById<TextView>(R.id.txt_password)
+        var message = findViewById<TextView>(R.id.txt_message)
+
+        var btn = findViewById<Button>(R.id.btn_login)
+
+        btn.setOnClickListener(){
+
+            //message.setText(password.toString())
+
+            if(username.equals("admin") && password.equals("admin123")){
+                message.setText("Login Success..!")
+            }else{
+                message.setText("Login Failed..")
+            }
+
+
+        }
     }
 }
