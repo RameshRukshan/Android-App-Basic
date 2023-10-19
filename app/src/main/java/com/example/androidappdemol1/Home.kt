b341da29d4a6f245c8881606881e587bef2657b9
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-10-19 15:43:03 +0530

    design and run config

--- app/src/main/java/com/example/androidappdemol1/Home.kt
+++ app/src/main/java/com/example/androidappdemol1/Home.kt
@@ -7,5 +7,10 @@ class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var nav_items = arrayOf("Home","About","Products","Contact")
+
+
+
     }
 }
