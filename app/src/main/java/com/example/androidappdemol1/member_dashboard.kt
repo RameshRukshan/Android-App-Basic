9525624fe58b42df8440c43ed5a4ff52694dfcdc
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-10-27 10:35:03 +0530

    Profile Section Done

--- app/src/main/java/com/example/androidappdemol1/member_dashboard.kt
+++ app/src/main/java/com/example/androidappdemol1/member_dashboard.kt
@@ -13,7 +13,11 @@ class member_dashboard : AppCompatActivity() {
         var output_lName = intent.getStringExtra("last_name")
 
         findViewById<TextView>(R.id.lbl_lastName).setText(output_lName)
-
         findViewById<TextView>(R.id.lbl_welcomeMessage).setText("Welcome $output_fName $output_lName to the Toyota")
+        findViewById<TextView>(R.id.lbl_full_name).setText("$output_fName $output_lName")
+
+        findViewById<TextView>(R.id.lbl_email).setText(intent.getStringExtra("email"))
+        findViewById<TextView>(R.id.lbl_tellNum).setText(intent.getStringExtra("tel"))
+
     }
 }
