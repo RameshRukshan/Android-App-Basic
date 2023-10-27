69a51f370614a9a0d4f2cae499a8950c7ddf6f0f
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-10-27 10:18:57 +0530

    home button clieck events

--- app/src/main/java/com/example/androidappdemol1/Home.kt
+++ app/src/main/java/com/example/androidappdemol1/Home.kt
@@ -25,7 +25,24 @@ class Home : AppCompatActivity() {
         btn_submit.setOnClickListener(){
             var to_mem_dashboard = Intent(this,member_dashboard::class.java)
 
+            var f_name = findViewById<EditText>(R.id.txt_fName).text.toString()
+            var l_name = findViewById<EditText>(R.id.txt_lName).text.toString()
+            var email = findViewById<EditText>(R.id.txt_email).text.toString()
+            var tel = findViewById<EditText>(R.id.txt_tel).text.toString()
+
+            to_mem_dashboard.putExtra("first_name",f_name)
+            to_mem_dashboard.putExtra("last_name",l_name)
+            to_mem_dashboard.putExtra("email",email)
+            to_mem_dashboard.putExtra("tel",tel)
+
             startActivity(to_mem_dashboard)
+        }
+
+        btn_clear.setOnClickListener(){
+            f_name.setText("First Name")
+            l_name.setText("Last Name")
+            email.setText("Email Address")
+            tel.setText("Telephone Number")
         }
     }
 }
