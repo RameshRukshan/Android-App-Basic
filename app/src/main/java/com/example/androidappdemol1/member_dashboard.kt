66211a11ece78f5b061a5308f8a398ff413bdb4c
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-10-27 10:25:00 +0530

    pass data between layers

--- app/src/main/java/com/example/androidappdemol1/member_dashboard.kt
+++ app/src/main/java/com/example/androidappdemol1/member_dashboard.kt
@@ -2,10 +2,18 @@ package com.example.androidappdemol1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class member_dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_member_dashboard)
+
+        var output_fName = intent.getStringExtra("first_name")
+        var output_lName = intent.getStringExtra("last_name")
+
+        findViewById<TextView>(R.id.lbl_lastName).setText(output_lName)
+
+        findViewById<TextView>(R.id.lbl_welcomeMessage).setText("Welcome $output_fName $output_lName to the Toyota")
     }
 }
